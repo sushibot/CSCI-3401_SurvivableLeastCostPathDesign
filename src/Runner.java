19898263c0c6d2a23303c96523c92e34060f451d
Tyler Atiburcio <tyler.atiburcio@gmail.com>
2016-04-13 10:17:19 -1000

    Structure of the project set
    Added Router/ Link class and placeholder class for implementation of
    routing all

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,28 @@
+
+import DataImport.DataInput;
+
 /**
  *
  * @author Tyler_Atiburcio
  */
 public class Runner {
+    
+    public static void main(String[] args) {
+        try{
+            if(args[0].toUpperCase().endsWith(".CSV"))
+            {
+                DataInput.read(args[0]);
+            }
+        }catch(Exception e)
+        {
+            System.err.println(e.getMessage());
+        }
+        finally{
+            System.out.println("\n\nUsage:");
+            System.out.println("Input: CSV file containing routers and their links");
+            System.out.println("CSCI-3401_SurvivableLeastCostDesignPathDesign.jar (fileName)\n");
+        }
+            
+    }
 
 }
