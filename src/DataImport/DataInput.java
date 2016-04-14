155ea9f55571e6e88fab00f3db472f09c6c6d3cd
Tyler Atiburcio <tyler.atiburcio@gmail.com>
2016-04-13 20:51:02 -1000

    Refined Data Input class
    Made changes according to data input

--- src/DataImport/DataInput.java
+++ src/DataImport/DataInput.java
@@ -15,8 +15,12 @@ public class DataInput {
         ArrayList<Router> routers = new ArrayList<Router>();
         File file = new File(arg);
         Scanner scan = new Scanner(file);
-        
-        
+        String tmp = scan.nextLine().toUpperCase();
+        if(!tmp.matches("[DEFINE|RAND]")) throw new Error("Invalid File header!");
+        while(scan.hasNext())
+        {
+            
+        }
         Router[] data = new  Router[routers.size()];
         for (int i = 0; i < data.length; i++) {
             data[i] = routers.get(i);
--- src/Router/Router.java
+++ src/Router/Router.java
@@ -10,5 +10,40 @@ import java.util.ArrayList;
 public class Router {
     
     private ArrayList<Link> links = new ArrayList<Link>();
+    protected String name;
+    /**Creates a new router with the given name
+     * 
+     * @param name Name of Router
+     */
+    public Router(String name)
+    {
+        this.name = name;
+    }
+    
+    /**Returns the name of the router
+     * 
+     * @return String Name of the Router
+     */
+    public String getName() {
+        return name;
+    }
+    
+    /**Sets the Router's name to a new name
+     * 
+     * @param name Sets name of router
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**Returns all the links of the router
+     * 
+     * @return ArrayList<Link> All the links to the router
+     */
+    public ArrayList<Link> getLinks() {
+        return links;
+    }
+    
+    
 
 }
