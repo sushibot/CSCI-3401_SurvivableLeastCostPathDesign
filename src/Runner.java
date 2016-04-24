86dee33404ca234d28028c6e94fd1420b380df8e
Tyler A <tyler.atiburcio@gmail.com>
2016-04-24 00:02:18 -1000

    Now link Disjoint

--- src/Runner.java
+++ src/Runner.java
@@ -21,8 +21,11 @@ public class Runner {
                 System.out.println("src " + routerList[0].getName());
                 System.out.println("dest " + routerList[routerList.length-1].getName());
                 Dijkstra_Alg test1 = new Dijkstra_Alg(routerList[0],routerList);
-                Path testPath = test1.runAlg(routerList[routerList.length-1])[0];
-                System.out.println(testPath);
+                Path[] testPath = test1.runAlg(routerList[routerList.length-1]);
+                System.out.println(testPath[0]);
+                //test1.printUsedLinks();
+                System.out.println(testPath[1]);
+                test1.printUsedLinks();
                 
             }
         }catch(Error e){
